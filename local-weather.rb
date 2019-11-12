d9d1ca93a97c735c0c654982056669ae422e93f2
goreleaserbot <goreleaser@carlosbecker.com>
2019-11-11 18:07:40 -0600

    Brew formula update for local-weather version v0.1.3

--- local-weather.rb
+++ local-weather.rb
@@ -2,16 +2,16 @@
 class LocalWeather < Formula
   desc "Lightweight CLI for quickly checking the current weather."
   homepage ""
-  version "0.1.2"
+  version "0.1.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/justinrsmith/local-weather-cli-go/releases/download/v0.1.2/local-weather_0.1.2_Darwin_x86_64.tar.gz"
-    sha256 "172e3f60b16cfa0ebd13a60ffa9e46ada2efb010283862e5d7ef70830bf5b845"
+    url "https://github.com/justinrsmith/local-weather-cli-go/releases/download/v0.1.3/local-weather_0.1.3_Darwin_x86_64.tar.gz"
+    sha256 "0205a748e7f7d8408a3113fe8481248682797637c4f703ad73283a00073fee96"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/justinrsmith/local-weather-cli-go/releases/download/v0.1.2/local-weather_0.1.2_Linux_x86_64.tar.gz"
-      sha256 "441c40b20bff560d943e23c2c3fcce0f71af4e8199eb5d5291c95867a427f43e"
+      url "https://github.com/justinrsmith/local-weather-cli-go/releases/download/v0.1.3/local-weather_0.1.3_Linux_x86_64.tar.gz"
+      sha256 "3a5337153449e24d71df75f3013e32312bbecd2e96eea2f7f96d43ac31fb91e5"
     end
   end
 
