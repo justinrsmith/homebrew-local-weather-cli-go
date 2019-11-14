5695a0e744f830e74c481257ece67a328bd85a30
goreleaserbot <goreleaser@carlosbecker.com>
2019-11-14 07:09:45 -0600

    Brew formula update for local-weather version v0.1.4

--- local-weather.rb
+++ local-weather.rb
@@ -7,11 +7,11 @@ class LocalWeather < Formula
 
   if OS.mac?
     url "https://github.com/justinrsmith/local-weather-cli-go/releases/download/v0.1.4/local-weather_0.1.4_Darwin_x86_64.tar.gz"
-    sha256 "833c135004c354604c597f139d75d400aaff33d2fe883d6f78dfac9e56ca9924"
+    sha256 "a7236b41b2b7c7824f3dc84c475ae49676b2617b880050e8747e8550bdd58238"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/justinrsmith/local-weather-cli-go/releases/download/v0.1.4/local-weather_0.1.4_Linux_x86_64.tar.gz"
-      sha256 "b3c14e20c46b051b95fcf76cb65c068cb368f97c67a515515180bd7537b16ced"
+      sha256 "31e5fa486b7d194525baf6ea7f528dc639728efa69d460d4f62815a569373b38"
     end
   end
 
